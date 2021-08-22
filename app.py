b5f41f0df9f079d2d45b0b88dc9258b27ef6bc0f
Vishal Patel <vishal.patel@sidewayseight.co.uk>
2021-08-22 23:17:26 +0100

    Database connection and test endpoints

---
+++ app.py
@@ -0,0 +1,83 @@
+from flask import Flask, jsonify
+import psycopg2
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+DATABASE_URL = os.getenv('DATABASE_URL')
+
+app = Flask(__name__)
+
+@app.route('/funded')
+def funded_last_ten():
+    connection = psycopg2.connect(dsn=DATABASE_URL)
+    cursor = connection.cursor()
+    cursor.execute("""
+        SELECT 
+            id, 
+            company_name, 
+            raise_date, 
+            raise_amount_mill_dollars::float, 
+            company_funding_round, 
+            company_website 
+        FROM raise
+        LIMIT 10;
+    """)
+    row = jsonify(cursor.fetchall())
+    cursor.close()
+    connection.close()
+
+    return row
+
+@app.route('/funded/<string:category>')
+def funded_by_category(category):
+    connection = psycopg2.connect(dsn=DATABASE_URL)
+    cursor = connection.cursor()
+    query = """
+        SELECT 
+            id, 
+            company_name, 
+            raise_date, 
+            raise_amount_mill_dollars::float, 
+            company_funding_round, 
+            company_website, 
+            company_category
+        FROM raise
+        WHERE company_category=%s
+        LIMIT 10;
+    """
+
+    cursor.execute(query, (category,))
+
+    row = jsonify(cursor.fetchall())
+    cursor.close()
+    connection.close()
+
+    return row
+
+@app.route('/funded/<string:company_name>')
+def funded_company(company_name):
+    connection = psycopg2.connect(dsn=DATABASE_URL)
+    cursor = connection.cursor()
+    query = """
+        SELECT 
+            id, 
+            company_name, 
+            raise_date, 
+            raise_amount_mill_dollars::float, 
+            company_funding_round, 
+            company_website 
+        FROM raise
+        WHERE company_name=%s;
+    """
+
+    cursor.execute(query, (company_name,))
+
+    row = jsonify(cursor.fetchall())
+    cursor.close()
+    connection.close()
+
+    return row
+
+if __name__ == '__main__':
+    app.run()
