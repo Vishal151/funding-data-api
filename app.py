3565b2055aec0df2fa502a8e910c6f34942f4990
Vishal Patel <vishal.patel@sidewayseight.co.uk>
2021-08-27 20:36:37 +0100

    Updated endpoint format with keys in JSON

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, Response
 import psycopg2
 from dotenv import load_dotenv
 import json
@@ -22,7 +22,7 @@ def funded_last_ten():
             company_funding_round, 
             company_website 
         FROM raise
-        LIMIT 10;
+        LIMIT 25;
     """)
     row_headers=[x[0] for x in cursor.description] #this will extract row headers
     row = cursor.fetchall()
@@ -31,10 +31,10 @@ def funded_last_ten():
             json_data.append(dict(zip(row_headers,result)))
     cursor.close()
     connection.close()
+    funded_data = json.dumps(json_data, default=str) # hack to jsonify datetime object
+    return Response(funded_data, mimetype='application/json') 
 
-    return json.dumps(json_data, default=str) # hack to jsonify datetime object
-
-@app.route('/funded/<string:category>')
+@app.route('/funded/category/<string:category>')
 def funded_by_category(category):
     connection = psycopg2.connect(dsn=DATABASE_URL)
     cursor = connection.cursor()
@@ -53,14 +53,20 @@ def funded_by_category(category):
     """
 
     cursor.execute(query, (category,))
+    # row = jsonify(cursor.fetchall())
 
-    row = jsonify(cursor.fetchall())
+    row_headers=[x[0] for x in cursor.description]
+    row = cursor.fetchall()
+    json_data=[]
+    for result in row:
+            json_data.append(dict(zip(row_headers,result)))
     cursor.close()
     connection.close()
 
-    return row
+    category_data = json.dumps(json_data, default=str) # hack to jsonify datetime object
+    return Response(category_data,  mimetype='application/json') 
 
-@app.route('/funded/<string:company_name>')
+@app.route('/funded/company/<string:company_name>')
 def funded_company(company_name):
     connection = psycopg2.connect(dsn=DATABASE_URL)
     cursor = connection.cursor()
@@ -78,11 +84,16 @@ def funded_company(company_name):
 
     cursor.execute(query, (company_name,))
 
-    row = jsonify(cursor.fetchall())
+    row_headers=[x[0] for x in cursor.description]
+    row = cursor.fetchall()
+    json_data=[]
+    for result in row:
+            json_data.append(dict(zip(row_headers,result)))
     cursor.close()
     connection.close()
 
-    return row
+    company_data = json.dumps(json_data, default=str) # hack to jsonify datetime object
+    return Response(company_data,  mimetype='application/json') 
 
 if __name__ == '__main__':
     app.run()
