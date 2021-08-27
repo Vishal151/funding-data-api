489a658f41a56eae893c280f47cfeae3f9229647
Vishal Patel <vishal.patel@sidewayseight.co.uk>
2021-08-27 17:09:52 +0100

    Added script used for DB table in PGAdmin

---
+++ db/script.sql
@@ -0,0 +1,22 @@
+
+-- Script to create table
+CREATE TABLE raise (
+	id INTEGER PRIMARY KEY,
+	raise_date DATE, 
+	company_name TEXT,
+	raise_amount_mill_dollars TEXT, 
+	company_hq_location TEXT,
+    raise_top_investors TEXT,
+	raise_link TEXT,
+	company_website TEXT,
+	company_funding_round TEXT,
+    company_category TEXT, 
+	company_description TEXT, 
+	company_expansion_plans TEXT,
+	founder_first_name TEXT,
+    founder_last_name TEXT,
+	founder_linkedin TEXT,
+	founder_twitter TEXT,
+	founder_angelist TEXT,
+	founder_email TEXT
+);
